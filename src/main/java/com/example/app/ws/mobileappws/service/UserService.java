02b90429482a5b0a5db819c4c6ad9afd6f09eded
Harish krishnan <harishkrishnan1993@gmail.com>
2021-01-03 12:38:02 +0530

    Add user service

---
+++ src/main/java/com/example/app/ws/mobileappws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.app.ws.mobileappws.service;
+
+import com.example.app.ws.mobileappws.shared.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
---
+++ src/main/java/com/example/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.app.ws.mobileappws.service.impl;
+
+import com.example.app.ws.mobileappws.service.UserService;
+import com.example.app.ws.mobileappws.shared.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+}
