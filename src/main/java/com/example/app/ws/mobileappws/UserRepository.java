269f6af36e142e3774a23c8e5bb8527feacd9c1f
Harish krishnan <harishkrishnan1993@gmail.com>
2021-01-04 21:02:20 +0530

    Add utils

---
+++ src/main/java/com/example/app/ws/mobileappws/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.app.ws.mobileappws;
+
+import com.example.app.ws.mobileappws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+}
--- src/main/java/com/example/app/ws/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -1,14 +1,42 @@
 package com.example.app.ws.mobileappws.service.impl;
 
+import com.example.app.ws.mobileappws.UserRepository;
+import com.example.app.ws.mobileappws.io.entity.UserEntity;
 import com.example.app.ws.mobileappws.service.UserService;
+import com.example.app.ws.mobileappws.shared.Utils;
 import com.example.app.ws.mobileappws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+
+        if (userRepository.findByEmail(userDto.getEmail()) != null)
+            throw new RuntimeException("Record already exists!");
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDto, userEntity);
+
+        String publicUserId = utils.generateUserId(30);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId(publicUserId);
+
+        UserEntity storedUserEntity = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserEntity, returnValue);
+
+        return returnValue;
     }
 }
